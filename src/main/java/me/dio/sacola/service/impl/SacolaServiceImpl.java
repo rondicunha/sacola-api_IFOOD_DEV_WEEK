6891c6f9a4882e27a18376be3b8e3d4f69ee86e9
Rondinelle Cunha <rondinelle.cunha.101@ufrn.edu.br>
2022-11-14 18:12:52 -0300

    Adicionando mais uma parte do projeto

---
+++ src/main/java/me/dio/sacola/service/impl/SacolaServiceImpl.java
@@ -0,0 +1,79 @@
+package me.dio.sacola.service.impl;
+
+import lombok.AllArgsConstructor;
+import me.dio.sacola.enumeration.FormaPagamento;
+import me.dio.sacola.model.Item;
+import me.dio.sacola.model.Restaurante;
+import me.dio.sacola.model.Sacola;
+import me.dio.sacola.model.repository.ItemRepository;
+import me.dio.sacola.model.repository.ProdutoRepository;
+import me.dio.sacola.model.repository.SacolaRepository;
+import me.dio.sacola.resource.dto.ItemDto;
+import me.dio.sacola.service.SacolaService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class SacolaServiceImpl implements SacolaService {
+
+    private final SacolaRepository sacolaRepository;
+    private final ProdutoRepository produtoRepository;
+    private final ItemRepository itemRepository;
+    @Override
+    public Item incluirItemNaSacola(ItemDto itemDto) {
+        Sacola sacola = verSacola(itemDto.getIdSacola());
+
+        if (sacola.isFechada()){
+            throw new RuntimeException("Esta sacola está fechada.");
+        }
+        Item itemParaSerInserido = Item.builder()
+                .quantidade(itemDto.getQuantidadeId())
+                .sacola(sacola)
+                .produto(produtoRepository.findById(itemDto.getProdutoId()).orElseThrow(() -> {
+                    throw new RuntimeException("Esse produto não existe.");
+                }))
+                .build();
+
+        List<Item> itensDaSacola = sacola.getItens();
+
+        if(itensDaSacola.isEmpty()){
+            itensDaSacola.add(itemParaSerInserido);
+        } else {
+            Restaurante restauranteAtual = itensDaSacola.get(0).getProduto().getRestaurante();
+            Restaurante restauranteDoItemParaAdicionar = itemParaSerInserido.getProduto().getRestaurante();
+            if(restauranteDoItemParaAdicionar.equals(restauranteAtual)){
+
+                itensDaSacola.add(itemParaSerInserido);
+            } else {
+
+                throw new RuntimeException("Não é possível adicionar produtos de restaurantes diferentes na mesma sacola.");
+            }
+        }
+
+        sacolaRepository.save(sacola);
+
+        return itemRepository.save(itemParaSerInserido);
+    }
+
+    @Override
+    public Sacola verSacola(Long id) {
+        return sacolaRepository.findById(id).orElseThrow( () -> { throw new RuntimeException ("Essa sacola não existe."); });
+    }
+
+    @Override
+    public Sacola fecharSacola(Long id, int numeroformaPagamento) {
+        Sacola sacola = verSacola(id);
+        if (sacola.getItens().isEmpty()){
+            throw  new RuntimeException("Inclua itens na sacola antes de fecha-la");
+        }
+
+        FormaPagamento formaPagamento = numeroformaPagamento == 0 ? FormaPagamento.DINHEIRO : FormaPagamento.MAQUINETA;
+
+        sacola.setFormaPagamento(formaPagamento);
+        sacola.setFechada(true);
+        return sacolaRepository.save(sacola);
+
+    }
+}
